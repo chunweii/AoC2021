082fa7c74a5e9c6872522b48d96bc24df8653421
chunweii <47494777+chunweii@users.noreply.github.com>
2021-12-28 02:02:22 +0800

    Add day 21

---
+++ day-21/solution.ts
@@ -0,0 +1,40 @@
+const winningScore = 1000;
+let playerPositions = [8, 7];
+let playerScores = [0, 0]
+let dieCounter = 0;
+
+function getDieNumberGenerator(): () => number {
+    let dieNumber = 0;
+    return () => {
+        dieCounter++;
+        let ans = ++dieNumber;
+        dieNumber %= 100;
+        return ans;
+    };
+}
+
+function movePlayer(n: number, p: number) {
+    playerPositions[p] += n - 1;
+    playerPositions[p] %= 10;
+    playerPositions[p] += 1;
+    playerScores[p] += playerPositions[p];
+}
+
+const getDieNumber = getDieNumberGenerator();
+
+// for (let i = 0; i < 102; i++) {
+//     console.log(getDieNumber());
+// }
+
+while (true) {
+    // player 1 plays
+    const numberOfMovesP1 = getDieNumber() + getDieNumber() + getDieNumber();
+    movePlayer(numberOfMovesP1, 0);
+    if (playerScores[0] >= winningScore) break;
+
+    const numberOfMovesP2 = getDieNumber() + getDieNumber() + getDieNumber();
+    movePlayer(numberOfMovesP2, 1);
+    if (playerScores[1] >= winningScore) break;
+}
+
+console.log(Math.min(...playerScores) * dieCounter);
---
+++ day-21/solution2.ts
@@ -0,0 +1,55 @@
+{
+const winningScore = 21;
+const playerPositions = [8, 7];
+const uniformDistribution = [[3, 1], [4, 3], [5, 6], [6, 7], [7, 6], [8, 3], [9, 1]];
+
+// mem[player1Pos][player2Pose][player1ScoreToWin][player2ScoreToWin][whoIsPlaying]
+// contains the pair [p1Winnings, p2Winnings]
+const mem: number[][][][][][]= []; 
+
+for (let i = 1; i <= 10; i++) {
+    const tempIArr: number[][][][][] = [];
+    for (let j = 1; j <= 10; j++) {
+        const tempJArr: number[][][][] = [];
+        for (let k = 0; k <= winningScore; k++) {
+            const tempKArr: number[][][] = [];
+            for (let l = 0; l <= winningScore; l++) {
+                tempKArr[l] = [];
+            }
+            tempJArr[k] = tempKArr;
+        }
+        tempIArr[j] = tempJArr;
+    }
+    mem[i] = tempIArr;
+}
+
+function movePlayer(n: number, p: number) {
+    p += n - 1;
+    p %= 10;
+    p += 1;
+    return p;
+}
+
+function solve(p1: number, p2: number, p1ToWin: number, p2ToWin: number, starting: number) {
+    if (p1ToWin <= 0) return [1, 0];
+    if (p2ToWin <= 0) return [0, 1];
+    if (mem[p1][p2][p1ToWin][p2ToWin][starting] === undefined) {
+        let result = [0, 0];
+        if (starting === 0) {
+            for (const [moves, freq] of uniformDistribution) {
+                const newPos = movePlayer(moves, p1);
+                solve(newPos, p2, p1ToWin - newPos, p2ToWin, 1).forEach((x, i) => result[i] += x * freq);
+            }
+        } else {
+            for (const [moves, freq] of uniformDistribution) {
+                const newPos = movePlayer(moves, p2);
+                solve(p1, newPos, p1ToWin, p2ToWin - newPos, 0).forEach((x, i) => result[i] += x * freq);
+            }
+        }
+        mem[p1][p2][p1ToWin][p2ToWin][starting] = result;
+    }
+    return mem[p1][p2][p1ToWin][p2ToWin][starting];
+}
+
+console.log(solve(playerPositions[0], playerPositions[1], winningScore, winningScore, 0));
+}
